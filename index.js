f3514174de3083fafe99efabd7a1d07816b42d82
Chris O'Hara <cohara87@gmail.com>
2012-10-16 16:07:56 +1100

    Test network functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Blog: require('./lib/blog').Blog
+  , Network: require('./lib/network').Network
   , FileSystemLoader: require('./lib/loaders/fs').FileSystemLoader
   , ArrayLoader: require('./lib/loaders/array').ArrayLoader
 };
---
+++ test/network.js
@@ -0,0 +1,127 @@
+var assert = require('assert')
+  , Blog = require('../').Blog
+  , Network = require('../').Network;
+
+describe('Network', function () {
+
+    it('should propagate blog loading errors', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { title: 'foo', date: '2012-10-01' }
+        ]));
+        network.add('barblog', new Blog([
+            { title: 'a' /* missing date */ }
+        ]));
+        network.load(function (err) {
+            if (!err) {
+                assert(false, 'No error on network load');
+            }
+            done();
+        });
+    });
+
+    it('should select posts aggregated from all blogs', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { title: 'foo', date: '2012-10-01' }
+          , { title: 'foo', date: '2012-10-03' }
+          , { title: 'bar', date: '2012-10-05' }
+        ]));
+        network.add('barblog', new Blog([
+            { title: 'a', date: '2012-10-02' }
+          , { title: 'b', date: '2012-10-04' }
+          , { title: 'c', date: '2012-10-06' }
+        ]));
+        network.load(function (err) {
+            assert(!err, err);
+            network.posts(function (err, selected) {
+                assert(!err, err);
+                assert.equal(selected.length, 6);
+                assert.equal(selected[0].title, 'c');
+                assert.equal(selected[1].title, 'bar');
+                assert.equal(selected[2].title, 'b');
+                assert.equal(selected[3].title, 'foo');
+                assert.equal(selected[4].title, 'a');
+                assert.equal(selected[5].title, 'foo');
+                done();
+            });
+        });
+    });
+
+    it('should select posts while respecting a limit and offset parameter', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { title: 'foo', date: '2012-10-01' }
+          , { title: 'foo', date: '2012-10-03' }
+          , { title: 'bar', date: '2012-10-05' }
+        ]));
+        network.add('barblog', new Blog([
+            { title: 'a', date: '2012-10-02' }
+          , { title: 'b', date: '2012-10-04' }
+          , { title: 'c', date: '2012-10-06' }
+        ]));
+        network.load(function (err) {
+            assert(!err, err);
+            network.posts(null, 3, 1, function (err, selected) {
+                assert(!err, err);
+                assert.equal(selected.length, 3);
+                assert.equal(selected[0].title, 'bar');
+                assert.equal(selected[0].blog, 'fooblog');
+                assert.equal(selected[1].title, 'b');
+                assert.equal(selected[1].blog, 'barblog');
+                assert.equal(selected[2].title, 'foo');
+                done();
+            });
+        });
+    });
+
+    it('should select posts using a query', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { title: 'foo', date: '2012-10-01', category: 'bar' }
+          , { title: 'foo', date: '2012-10-03' }
+          , { title: 'bar', date: '2012-10-05' }
+        ]));
+        network.add('barblog', new Blog([
+            { title: 'a', date: '2012-10-02' }
+          , { title: 'b', date: '2012-10-04', category: 'bar' }
+          , { title: 'c', date: '2012-10-06' }
+        ]));
+        network.load(function (err) {
+            assert(!err, err);
+            network.posts({ category: 'bar' }, function (err, selected) {
+                assert(!err, err);
+                assert.equal(selected.length, 2);
+                assert.equal(selected[0].title, 'b');
+                assert.equal(selected[1].title, 'foo');
+                selected.forEach(function (post) {
+                    assert.equal(post.category, 'bar');
+                });
+                done();
+            });
+        });
+    });
+
+    it('should select a unique set of keys from each post', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { title: 'foo', date: '2012-10-01', category: 'bar' }
+          , { title: 'foo', date: '2012-10-03', category: 'bar' }
+          , { title: 'bar', date: '2012-10-05', category: 'foo' }
+        ]));
+        network.add('barblog', new Blog([
+            { title: 'a', date: '2012-10-02', category: 'foobar' }
+          , { title: 'b', date: '2012-10-04', category: 'bar' }
+          , { title: 'c', date: '2012-10-06', category: 'foobar' }
+        ]));
+        network.load(function (err) {
+            assert(!err, err);
+            assert.deepEqual(network.keys('category'), ['foo', 'bar', 'foobar']);
+            //Check the cached response
+            assert.deepEqual(network.keys('category'), ['foo', 'bar', 'foobar']);
+            done();
+        });
+    });
+
+});
+

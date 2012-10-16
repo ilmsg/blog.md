0b16c5e9743129b5f9395aa01119a443ada8da44
Chris O'Hara <cohara87@gmail.com>
2012-10-17 10:26:43 +1100

    Load a wordpress blog

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
   , Network: require('./lib/network').Network
   , FileSystemLoader: require('./lib/loaders/fs').FileSystemLoader
   , ArrayLoader: require('./lib/loaders/array').ArrayLoader
+  , WordpressLoader: require('./lib/loaders/wordpress').WordpressLoader
 };
 

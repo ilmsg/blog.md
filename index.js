2f6cb0557baa2a7d0dc1f94afc4c0d64d73e95d3
Chris O'Hara <cohara87@gmail.com>
2012-11-05 11:41:54 +1100

    Remove the wordpress loader

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
   , Network: require('./lib/network').Network
   , FileSystemLoader: require('./lib/loaders/fs').FileSystemLoader
   , ArrayLoader: require('./lib/loaders/array').ArrayLoader
-  , WordpressLoader: require('./lib/loaders/wordpress').WordpressLoader
   , middleware: require('./lib/middleware')
 };
 

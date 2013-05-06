01660b7c2bb991dae74d42bf831c04ec97cd1dd2
Chris O'Hara <cohara87@gmail.com>
2013-05-06 11:04:26 +1000

    Supported nested object queries

--- test/utils.js
+++ test/utils.js
@@ -5,15 +5,20 @@ describe('Utils', function () {
 
     it('should provide a generic query facility', function () {
         var obj = { foo: 'bar' };
+        assert.ok(utils.query(obj, { foo: 'bar' }));
+        assert.ok(utils.query(obj, { foo: /bar/ }));
+        assert.ok(utils.query(obj, { foo: [ 'bar', 'baz' ] }));
+        assert.ok(utils.query(obj, { foo: [ /bar/, /baz/ ] }));
+        assert.ok(!utils.query(obj, { foo: [ 'foobar', 'baz' ] }));
+        assert.ok(utils.query(obj, { foo: { $not: 'baz' } }));
+        assert.ok(utils.query(obj, { foo: { $not: [ 'baz', 'foobar' ] } }));
+        assert.ok(!utils.query(obj, { foo: { $not: [ 'baz', 'bar' ] } }));
+    });
 
-        assert.ok(utils.query(obj, { foo: 'bar' }))
-        assert.ok(utils.query(obj, { foo: /bar/ }))
-        assert.ok(utils.query(obj, { foo: [ 'bar', 'baz' ] }))
-        assert.ok(utils.query(obj, { foo: [ /bar/, /baz/ ] }))
-        assert.ok(!utils.query(obj, { foo: [ 'foobar', 'baz' ] }))
-        assert.ok(utils.query(obj, { foo: { $not: 'baz' } }))
-        assert.ok(utils.query(obj, { foo: { $not: [ 'baz', 'foobar' ] } }))
-        assert.ok(!utils.query(obj, { foo: { $not: [ 'baz', 'bar' ] } }))
+    it('should support nested obj queries', function () {
+        var obj = { foo: 'bar', category: { baz: 'qux' } };
+        assert.ok(!utils.query(obj, { category: { baz: 'foo' } }));
+        assert.ok(utils.query(obj, { category: { baz: 'qux' } }));
     });
 
 });

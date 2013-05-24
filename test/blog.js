d059ae455f7c26f32e99c19334a1499ec5611640
Chris O'Hara <cohara87@gmail.com>
2013-05-24 14:31:20 +1000

    Remove onload

--- test/blog.js
+++ test/blog.js
@@ -214,30 +214,6 @@ describe('Blog', function () {
         });
     });
 
-    it('should run a function when data changes', function (done) {
-        var blog = new Blog([
-            { title: 'foo', date: '2012-10-01', category: 'bar' }
-          , { title: 'foo', date: '2012-10-02', category: 'foobar' }
-          , { title: 'bar', date: '2012-10-03', category: 'foobar' }
-          , { title: 'baz', date: '2012-10-04', category: 'baz' }
-        ]);
-        blog.onload(function (blog, posts) {
-            blog.foo = 'bar';
-            posts.forEach(function (post) {
-                post.bar = 'foo';
-            });
-        });
-        blog.load(function (err) {
-            assert(!err, err);
-            assert.equal(blog.metadata.foo, 'bar');
-            blog.post('bar', function (err, post) {
-                assert(!err, err);
-                assert.equal(post.bar, 'foo');
-                done();
-            });
-        });
-    });
-
     it('should maintain an internal linked list to get prev/next posts', function (done) {
         var blog = new Blog([
             { title: 'foo1', date: '2012-10-01', category: 'bar' }

46092998c7530b04d7b34c6b2d2b6bacd90eda4b
Chris O'Hara <cohara87@gmail.com>
2013-05-25 18:29:01 +1000

    Watch the filesystem for changes

--- test/fs.js
+++ test/fs.js
@@ -1,7 +1,11 @@
 var lib_dir = process.env.JS_COV ? '../lib-cov/': '../lib/';
 
 var assert = require('assert')
-  , FileSystemLoader = require(lib_dir + 'loaders/fs').FileSystemLoader;
+  , FileSystemLoader = require(lib_dir + 'loaders/fs').FileSystemLoader
+  , Blog = require(lib_dir + 'blog').Blog
+  , fs = require('fs');
+
+var watch_timeout = 300;
 
 describe('File System Loader', function () {
 
@@ -61,13 +65,81 @@ describe('File System Loader', function () {
         });
     });
 
-    it('should detect new blog posts and push automatically');
+    it('should detect new blog posts', function (done) {
+        fs.unlink(__dirname + '/data/blog4/a/post2.md', function () {
+            var loader = new FileSystemLoader(__dirname + '/data/blog4')
+              , blog = new Blog(loader);
+            blog.on('load', function () {
+                var posts = blog.select();
+                assert.equal(posts.length, 1);
+                assert.equal(posts[0].title, 'post1');
+                FileSystemLoader.save(__dirname + '/data/blog4/a/post2.md', {
+                    title: 'foo'
+                  , body: 'Foobar'
+                  , date: '2012-10-01'
+                });
+                setTimeout(function () {
+                    var posts = blog.select();
+                    assert.equal(posts.length, 2);
+                    var post = blog.post('foo');
+                    assert.equal(post.body, '<p>Foobar</p>');
+                    done();
+                }, watch_timeout);
+            });
+        });
+    });
 
-    it('should push changes to blog posts');
+    it('should detect changes to blog posts', function (done) {
+        fs.unlink(__dirname + '/data/blog4/a/post2.md', function () {
+            FileSystemLoader.save(__dirname + '/data/blog4/a/post2.md', {
+                title: 'foo'
+              , body: 'Foobar'
+              , date: '2012-10-01'
+            });
+            var loader = new FileSystemLoader(__dirname + '/data/blog4')
+              , blog = new Blog(loader);
+            blog.on('load', function () {
+                var post = blog.post('foo');
+                assert.equal(post.body, '<p>Foobar</p>');
+                FileSystemLoader.save(__dirname + '/data/blog4/a/post2.md', {
+                    title: 'foo'
+                  , body: 'baz'
+                  , date: '2012-10-01'
+                });
+                setTimeout(function () {
+                    var posts = blog.select();
+                    assert.equal(posts.length, 2);
+                    var post = blog.post('foo');
+                    assert.equal(post.body, '<p>baz</p>');
+                    done();
+                }, watch_timeout);
+            });
+        });
+    });
 
-    it('should detect when a blog post is removed');
-
-    it('should detect changes to blog metadata');
+    it('should detect when a blog post is removed', function (done) {
+        fs.unlink(__dirname + '/data/blog4/a/post2.md', function () {
+            FileSystemLoader.save(__dirname + '/data/blog4/a/post2.md', {
+                title: 'foo'
+              , body: 'Foobar'
+              , date: '2012-10-01'
+            });
+            var loader = new FileSystemLoader(__dirname + '/data/blog4')
+              , blog = new Blog(loader);
+            blog.on('load', function () {
+                var post = blog.post('foo');
+                assert.equal(post.body, '<p>Foobar</p>');
+                assert.equal(blog.count(), 2);
+                fs.unlink(__dirname + '/data/blog4/a/post2.md', function () {
+                    setTimeout(function () {
+                        assert.equal(blog.count(), 1);
+                        assert(!blog.post('foo'));
+                        done();
+                    }, watch_timeout);
+                });
+            });
+        });
+    });
 
 });
 
